90367383d3c2cea148e0284da68cb7acdfd69e29
Ghodasara <chetan.ghodasara@p36019.intra.rakuten.co.jp>
2017-08-21 16:13:29 +0900

    Initial Commit

---
+++ src/main/java/com/chetan/demomsta/DemomstaApplication.java
@@ -0,0 +1,12 @@
+package com.chetan.demomsta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomstaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomstaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/chetan/demomsta/DemomstaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chetan.demomsta;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemomstaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
